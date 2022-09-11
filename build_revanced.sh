7785ffcc480bf92afe0cb839528ee1e5ce597f07
MrHook404 <113281640+MrHook404@users.noreply.github.com>
2022-09-11 02:54:43 -0500

    Update build_revanced.sh
    Signed-off-by: MrHook404 <113281640+MrHook404@users.noreply.github.com>

--- build_revanced.sh
+++ build_revanced.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 #F1R3W4LL404_Version
-VMG_VERSION="0.2.24.220220"
 
 declare -A artifacts
 
 artifacts["revanced-cli.jar"]="revanced/revanced-cli revanced-cli .jar"
 artifacts["revanced-integrations.apk"]="revanced/revanced-integrations app-release-unsigned .apk"
 artifacts["revanced-patches.jar"]="FirewallSH56/YoutubeAdvanced-patches revanced-patches .jar"
-artifacts["apkeep"]="EFForg/apkeep apkeep-x86_64-unknown-linux-gnu"
 
 get_artifact_download_url () {
     local api_url="https://api.github.com/repos/$1/releases/latest"
@@ -22,32 +20,17 @@ for artifact in "${!artifacts[@]}"; do
     fi
 done
 
-
-chmod +x apkeep
-
-
-if [ ! -f "vanced-microG.apk" ]; then
-    echo "Downloading Vanced microG"
-    ./apkeep -a com.mgoogle.android.gms@$VMG_VERSION .
-    mv com.mgoogle.android.gms@$VMG_VERSION.apk vanced-microG.apk
-fi
-
-
 echo "************************************"
-echo "Building YouTube APK"
+echo "Building Tiktok APK"
 echo "************************************"
 
 mkdir -p build
 
-if [ -f "com.google.android.youtube.apk" ]
+if [ -f "com.zhiliaoapp.musically.apk" ]
 then
-    echo "Building Root APK"
-    java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar --mount \
-                               -e microg-support \
-                               -a com.google.android.youtube.apk -o build/revanced-root.apk
     echo "Building Non-root APK"
     java -jar revanced-cli.jar -m revanced-integrations.apk -b revanced-patches.jar  \
-                               -a com.google.android.youtube.apk -o build/revanced-nonroot.apk
+                               -a com.zhiliaoapp.musically.apk.apk -o build/Tiktok-nonroot.apk
 else
-    echo "Cannot find YouTube APK, skipping build"
+    echo "Cannot find Tiktok APK, skipping build"
 fi
